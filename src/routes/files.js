de542b59b700cdd7c6c78b80eb9dd91dfa6906a9
onfafanutifafa <fafanutifafa9@gmail.com>
2026-06-25 02:18:07 +0000

    getdebug: Reflected Cross-Site Scripting (XSS) via /files/preview

--- src/routes/files.js
+++ src/routes/files.js
@@ -11,6 +11,15 @@ const { requireUser } = require("../middleware/auth");
 
 const router = express.Router();
 
+function escapeHtml(unsafe) {
+  return unsafe
+    .replace(/&/g, "&amp;")
+    .replace(/</g, "&lt;")
+    .replace(/>/g, "&gt;")
+    .replace(/"/g, "&quot;")
+    .replace(/'/g, "&#039;");
+}
+
 router.get("/files/download", requireUser, (req, res) => {
   const data = files.readUserFile(req.query.path || "");
   res.json({ bytes: data.length });
@@ -25,15 +34,6 @@ router.post("/files/thumbnail", requireUser, async (req, res) => {
   const code = await files.makeThumbnail(req.body.name);
   res.json({ exit_code: code });
 });
-
-// Helper function to HTML-escape a string
-function escapeHtml(str) {
-  return str.replace(/&/g, "&amp;")
-            .replace(/</g, "&lt;")
-            .replace(/>/g, "&gt;")
-            .replace(/"/g, "&quot;")
-            .replace(/'/g, "&#039;");
-}
 
 router.get("/files/preview", requireUser, (req, res) => {
   const caption = req.query.caption || "";
