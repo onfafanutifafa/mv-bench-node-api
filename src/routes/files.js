29d8165639332931c9afe1c9be00033069eafea2
onfafanutifafa <fafanutifafa9@gmail.com>
2026-06-28 11:14:59 +0000

    getdebug: Reflected XSS in File Preview

--- src/routes/files.js
+++ src/routes/files.js
@@ -11,6 +11,15 @@ const { requireUser } = require("../middleware/auth");
 
 const router = express.Router();
 
+// Helper function to HTML-escape strings
+function escapeHtml(str) {
+  return str.replace(/&/g, "&amp;")
+            .replace(/</g, "&lt;")
+            .replace(/>/g, "&gt;")
+            .replace(/"/g, "&quot;")
+            .replace(/'/g, "&#039;");
+}
+
 router.get("/files/download", requireUser, (req, res) => {
   const data = files.readUserFile(req.query.path || "");
   res.json({ bytes: data.length });
@@ -29,7 +38,7 @@ router.post("/files/thumbnail", requireUser, async (req, res) => {
 router.get("/files/preview", requireUser, (req, res) => {
   const caption = req.query.caption || "";
   res.set("Content-Type", "text/html");
-  res.send("<div class=\"preview\"><h2>Preview</h2><p>" + caption + "</p></div>");
+  res.send("<div class=\"preview\"><h2>Preview</h2><p>" + escapeHtml(caption) + "</p></div>");
 });
 
 module.exports = router;
